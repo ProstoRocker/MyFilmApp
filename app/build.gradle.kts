6b8260645d4d0d6d2a8474ec3297597e6ebab180
ProstoRocker <SaberSai608@gmail.com>
2025-11-14 17:55:48 +0500

    Add Menu buttons; Insert movie posters; Insert text fields; Apply Drawable to backgrounds; Apply CardView for rounded corners and levitation; Add functionality with Toast messages at the click of each button. **In progress: Create resources for different displays using qualifiers.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,5 +61,7 @@ dependencies {
 ///implement
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.11.0")
 
 }
--- app/src/main/java/com/ilyadev/moviesearch/MainActivity.kt
+++ app/src/main/java/com/ilyadev/moviesearch/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.ilyadev.moviesearch
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Находим кнопки
+        val btnMenu = findViewById<Button>(R.id.btn_menu)
+        val btnFavorites = findViewById<Button>(R.id.btn_favorites)
+        val btnLater = findViewById<Button>(R.id.btn_later)
+        val btnCollections = findViewById<Button>(R.id.btn_collections)
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
+
+        // Обработчики
+        btnMenu.setOnClickListener { Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show() }
+        btnFavorites.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        btnLater.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        btnCollections.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        btnSettings.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
